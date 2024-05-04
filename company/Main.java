de9e30cb111b0bc2f4d078399ec1519fe60f25d0
Norbert Kloc <norb906@gmail.com>
2024-05-04 11:32:48 +0200

    Message

---
+++ company/Main.java
@@ -0,0 +1,16 @@
+package company;
+
+import company.utils.MathUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        
+        int sum = MathUtils.add(num1, num2);
+        System.out.println("Wynik dodawania: " + sum);
+        
+        
+    }
+    
+}
